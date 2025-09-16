aed0a7262d20dd3c803273e35d0befa832cd7cdf
NoamCohen54 <152982236+NoamCohen54@users.noreply.github.com>
2025-09-16 18:44:28 +0000

    adding 3 violation to CourseForm.tsx

--- my-degree-tracker/src/Forms/CourseForm.tsx
+++ my-degree-tracker/src/Forms/CourseForm.tsx
@@ -16,90 +16,102 @@ const CourseForm: React.FC = () => {
     isEdit,
   } = useCourseForm();
 
+  // Violation 3: Components Best Practices – unrelated logic
+  const randomNumber = Math.random();
+
   return (
-    <Box sx={{ maxWidth: 600, mx: "auto", mt: 4 }}>
-      <Typography variant="h6" gutterBottom>
-        {isEdit ? "Edit Course" : "Add Course"}
-      </Typography>
+    // Violation 2: HTML Best Practices – unnecessary <div> wrapper
+    <div>
+      {/* Violation 1: CSS Best Practices – inline style */}
+      <Box style={{ maxWidth: 600, margin: "0 auto" }}>
+        <Typography variant="h6" gutterBottom>
+          {isEdit ? "Edit Course" : "Add Course"}
+        </Typography>
 
-      <TextField
-        label="Course Code"
-        value={data.courseCode}
-        onChange={handleChange("courseCode")}
-        error={!!errors.courseCode}
-        helperText={errors.courseCode}
-        required
-        fullWidth
-        margin="normal"
-        disabled={isEdit}
-      />
+        {/* unrelated debug value */}
+        <Typography variant="body2" color="error">
+          Debug Value: {randomNumber}
+        </Typography>
 
-      <TextField
-        label="Course Name"
-        value={data.courseName}
-        onChange={handleChange("courseName")}
-        error={!!errors.courseName}
-        helperText={errors.courseName}
-        required
-        fullWidth
-        margin="normal"
-      />
+        <TextField
+          label="Course Code"
+          value={data.courseCode}
+          onChange={handleChange("courseCode")}
+          error={!!errors.courseCode}
+          helperText={errors.courseCode}
+          required
+          fullWidth
+          margin="normal"
+          disabled={isEdit}
+        />
 
-      <TextField
-        type="number"
-        inputProps={{ min: 1 }}
-        label="Credits"
-        value={data.credits}
-        onChange={handleChange("credits")}
-        error={!!errors.credits}
-        helperText={errors.credits}
-        required
-        fullWidth
-        margin="normal"
-      />
+        <TextField
+          label="Course Name"
+          value={data.courseName}
+          onChange={handleChange("courseName")}
+          error={!!errors.courseName}
+          helperText={errors.courseName}
+          required
+          fullWidth
+          margin="normal"
+        />
 
-      <TextField
-        select
-        label="Semester"
-        value={data.semester}
-        onChange={handleChange("semester")}
-        error={!!errors.semester}
-        helperText={errors.semester}
-        required
-        fullWidth
-        margin="normal"
-      >
-        <MenuItem value="A">A</MenuItem>
-        <MenuItem value="B">B</MenuItem>
-        <MenuItem value="C">C</MenuItem>
-      </TextField>
+        <TextField
+          type="number"
+          inputProps={{ min: 1 }}
+          label="Credits"
+          value={data.credits}
+          onChange={handleChange("credits")}
+          error={!!errors.credits}
+          helperText={errors.credits}
+          required
+          fullWidth
+          margin="normal"
+        />
 
-      <TextField
-        label="Assignments (comma separated)"
-        value={data.assignments}
-        onChange={handleChange("assignments")}
-        error={!!errors.assignments}
-        helperText={errors.assignments}
-        required
-        fullWidth
-        margin="normal"
-        multiline
-        minRows={2}
-      />
+        <TextField
+          select
+          label="Semester"
+          value={data.semester}
+          onChange={handleChange("semester")}
+          error={!!errors.semester}
+          helperText={errors.semester}
+          required
+          fullWidth
+          margin="normal"
+        >
+          <MenuItem value="A">A</MenuItem>
+          <MenuItem value="B">B</MenuItem>
+          <MenuItem value="C">C</MenuItem>
+        </TextField>
 
-      <Box mt={2} textAlign="right">
-        <Button variant="contained" onClick={handleSubmit}>
-          {isEdit ? "Update" : "Save"}
-        </Button>
+        <TextField
+          label="Assignments (comma separated)"
+          value={data.assignments}
+          onChange={handleChange("assignments")}
+          error={!!errors.assignments}
+          helperText={errors.assignments}
+          required
+          fullWidth
+          margin="normal"
+          multiline
+          minRows={2}
+        />
+
+        <Box mt={2} textAlign="right">
+          <Button variant="contained" onClick={handleSubmit}>
+            {isEdit ? "Update" : "Save"}
+          </Button>
+        </Box>
+
+        <SnackbarNotification
+          open={snackOpen}
+          severity={snackSeverity}
+          message={snackMsg}
+          onClose={() => setSnackOpen(false)}
+        />
       </Box>
-
-      <SnackbarNotification
-        open={snackOpen}
-        severity={snackSeverity}
-        message={snackMsg}
-        onClose={() => setSnackOpen(false)}
-      />
-    </Box>
+    </div>
   );
 };
 
